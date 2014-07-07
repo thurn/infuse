2865c4875da17fd6550af119fe057b062b32c30d
Derek Thurn <derek@fake.email>
2014-07-07 08:08:08 -0700

    update javadoc

--- src/ca/thurn/infuse/Injector.java
+++ src/ca/thurn/infuse/Injector.java
@@ -34,11 +34,38 @@ public class Injector {
    *     allowed, but Initializers are Module-scoped, so things like singletons
    *     will still only be created once.
    * @return A newly created Injector wrapping the supplied modules.
+   * @throws IllegalStateException If multiple bindings are supplied for a
+   *     single key.
    */
   public static Injector create(Module... modules) {
     return new Injector(Arrays.asList(modules));
   }
 
+  /**
+   * Retrieves a value by running its associated initializer.
+   *
+   * <p>Example use:</p>
+   * <pre>{@code
+   * final class Thermosiphon implements Pump {
+   *  private final Heater heater;
+   *   Thermosiphon(Injector injector) {
+   *     heater = injector.get(Heater.class);
+   *   }
+   * }
+   * }</pre>
+   *
+   * <p>This example shows how to retrieve dependencies from an Injector which
+   * is the first constructor argument to a class. This is the suggested
+   * pattern for using an Injector to create an object. Refer to the javadoc
+   * for {@link Module} for a corresponding Module definition involving this
+   * class.</p>
+   *
+   * @param key The class object key associated with the desired value.
+   * @return The value bound to this key. By default, this is a fresh instance
+   *     created by running the supplied initializer. For singleton keys, the
+   *     same value is always returned.
+   * @throws IllegalArgumentException if no value was bound to this key.
+   */
   public <T> T get(Class<T> key) {
     return wrapperModule.runInitializer(this, key);
   }
--- src/ca/thurn/infuse/Module.java
+++ src/ca/thurn/infuse/Module.java
@@ -3,6 +3,29 @@ package ca.thurn.infuse;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * The base class which you need to extend from in order to bind keys to
+ * initializers for your application. Only one initializer should be supplied
+ * for each key.
+ *
+ * <p>Example module:</p>
+ *
+ * <pre>{@code
+ * final class PumpModule extends Module {
+ *   @Override public void configure() {
+ *     bindKey(Pump.class, new Initializer<Pump>() {
+ *       @Override public Pump initialize(Injector injector) {
+ *         return new Thermosiphon(injector);
+ *       }
+ *     });
+ *   }
+ * }
+ * }</pre>
+ *
+ * <p>This is an example of a module which binds a concrete value
+ * (Thermosiphon) to an interface (Pump). A new Thermosiphon will be created
+ * every time a Pump is requested.</p>
+ */
 public abstract class Module {
   private static final class CachingInitializer<T> {
     private final Initializer<T> initializer;
@@ -29,21 +52,35 @@ public abstract class Module {
   private final Map<Class<?>, CachingInitializer<?>> initializers =
       new HashMap<Class<?>, CachingInitializer<?>>();
 
+  /**
+   * Binds keys to values for later retrieval by an {@link Injector}. Keys
+   * should be associated with an initializer function via
+   * {@link Module#bindKey(Class, Initializer)} and related methods. Modules
+   * can also import all of the bindings defined in another module by calling
+   * {@link Module#includeModule(Module)}.
+   */
   public abstract void configure();
 
   @SuppressWarnings("unchecked")
   <T> T runInitializer(Injector injector, Class<T> key) {
     try {
+      // This unchecked cast should be safe because it's only possible to bind
+      // a key of type T to an initializer of type T in bindKey.
       return (T) initializers.get(key).getResult(injector);
     } catch (NullPointerException exception) {
       throw new IllegalArgumentException("No instance bound for key '" + key + "'");
-    } catch (ClassCastException exception) {
-      throw new IllegalStateException("Initializer for key '" + key +
-          "' did not return an instance of the correct type.\n" + exception.getMessage());
     }
   }
 
-  public void includeModule(Module module) {
+  /**
+   * Merges all of the bindings defined in another module into this module.
+   * This will invoke the other module's {@link Module#configure()} method.
+   *
+   * @param module The module to configure and then merge bindings from.
+   * @throws IllegalStateException If this module defines a key which a binding
+   *     has already been configured for.
+   */
+  public final void includeModule(Module module) {
     module.configure();
     for (Class<?> key : module.initializers.keySet()) {
       if (initializers.containsKey(key)) {
@@ -55,11 +92,29 @@ public abstract class Module {
     }
   }
 
+  /**
+   * Similar to {@link Module#bindKey(Class, Initializer)}, except the
+   * initializer function will only be invoked once. Subsequent calls to the
+   * {@link Injector#get(Class)} method will return the same value.
+   *
+   * @see Module#bindKey(Class, Initializer)
+   */
   public final <T> void bindSingletonKey(Class<T> key, Initializer<T> initializer) {
     checkForDuplicate(key);
     initializers.put(key, new CachingInitializer<T>(initializer, true /* isSingleton */));
   }
 
+  /**
+   * Associates a key with an initializer function. This initializer function
+   * will be invoked every time a value is requested for the provided key via
+   * {@link Injector#get(Class)}.
+   *
+   * @param key The key to bind.
+   * @param initializer An initializer function which will be invoked every
+   *     time a value is requested for the provided key.
+   *  @throws IllegalStateException If an initializer has already been bound to
+   *      this key.
+   */
   public final <T> void bindKey(Class<T> key, Initializer<T> initializer) {
     checkForDuplicate(key);
     initializers.put(key, new CachingInitializer<T>(initializer, false /* isSingleton */));
